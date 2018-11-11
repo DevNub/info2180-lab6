5045bcf9564152495925ad379daad8548e4beb98
DevNub <ddac_dan@hotmail.com>
2018-11-11 11:00:03 -0600

    Exercise 2

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 window.onload = function(){
 
+//var $;
 var sb = document.getElementById("search");
 var httpRequest = new XMLHttpRequest();
+var term = "";
+var q = "?q="
 var url = "";
+var results = document.getElementById("sResults");
+
     
     sb.onclick = function(){
         url = "https://info2180-lab6-devnub.c9users.io/request.php"
+        term = document.getElementById("sTerm").value;
         httpRequest.onreadystatechange = sbClick;
-        httpRequest.open('GET', url)
+        httpRequest.open('GET', url+q+term);
         httpRequest.send();
     }
     
@@ -15,6 +21,12 @@ var url = "";
         if(httpRequest.readyState === XMLHttpRequest.DONE){
             if(httpRequest.status === 200){
                 var response = httpRequest.responseText;
+                
+                response = response.replace("<h3>","");
+                response = response.replace("</h3>","");
+                response = response.replace("<p>","  \n");
+                response = response.replace("</p>","");
+                
                 alert(response);
             }
             else{
