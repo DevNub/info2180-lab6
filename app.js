b6c60c24499e25ea06d72f893034ce12005dbb6a
DevNub <ddac_dan@hotmail.com>
2018-11-11 11:51:28 -0600

    Exercise 3

--- app.js
+++ app.js
@@ -6,33 +6,37 @@ var httpRequest = new XMLHttpRequest();
 var term = "";
 var q = "?q="
 var url = "";
-var results = document.getElementById("sResults");
+var results = document.getElementById("result");
+var response = "";
 
     
     sb.onclick = function(){
         url = "https://info2180-lab6-devnub.c9users.io/request.php"
-        term = document.getElementById("sTerm").value;
+        
         httpRequest.onreadystatechange = sbClick;
-        httpRequest.open('GET', url+q+term);
+        term = "?q=";
+        term += document.getElementById("sTerm").value;
+        url += term;
+        httpRequest.open('GET', url);//+q+term);
         httpRequest.send();
+        return false;
     }
     
     function sbClick(){
         if(httpRequest.readyState === XMLHttpRequest.DONE){
             if(httpRequest.status === 200){
                 var response = httpRequest.responseText;
+                var resp = response;
+                resp = resp.replace("<h3>","");
+                resp = resp.replace("</h3>","");
+                resp = resp.replace("<p>","  \n");
+                resp = resp.replace("</p>","");
                 
-                response = response.replace("<h3>","");
-                response = response.replace("</h3>","");
-                response = response.replace("<p>","  \n");
-                response = response.replace("</p>","");
-                
-                alert(response);
+                results.innerHTML = response;
             }
             else{
                 alert('Problem with your request');
             }
         }
-        
     }
 }
